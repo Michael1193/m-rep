ca5b802adafcedf3243e2c63c0f95fa2ad96e7e6
Michael <go99@bk.ru>
2017-08-06 18:18:13 +0300

    замигал диод

---
+++ dir/build/dep/crt0.o.d
@@ -0,0 +1,321 @@
+build/obj/crt0.o: src/crt0.c libchip/include/samv71/samv71.h \
+ libchip/include/samv71/samv71q21.h \
+ /home/admin1/arm_toolchain/gcc-arm-none-eabi-6-2017-q2-update/lib/gcc/arm-none-eabi/6.3.1/include/stdint.h \
+ /home/admin1/arm_toolchain/gcc-arm-none-eabi-6-2017-q2-update/arm-none-eabi/include/stdint.h \
+ /home/admin1/arm_toolchain/gcc-arm-none-eabi-6-2017-q2-update/arm-none-eabi/include/machine/_default_types.h \
+ /home/admin1/arm_toolchain/gcc-arm-none-eabi-6-2017-q2-update/arm-none-eabi/include/sys/features.h \
+ /home/admin1/arm_toolchain/gcc-arm-none-eabi-6-2017-q2-update/arm-none-eabi/include/_newlib_version.h \
+ /home/admin1/arm_toolchain/gcc-arm-none-eabi-6-2017-q2-update/arm-none-eabi/include/sys/_intsup.h \
+ /home/admin1/arm_toolchain/gcc-arm-none-eabi-6-2017-q2-update/arm-none-eabi/include/sys/_stdint.h \
+ libchip/include/cmsis/CMSIS/Include/core_cm7.h \
+ libchip/include/cmsis/CMSIS/Include/core_cmInstr.h \
+ libchip/include/cmsis/CMSIS/Include/cmsis_gcc.h \
+ libchip/include/cmsis/CMSIS/Include/core_cmFunc.h \
+ libchip/include/cmsis/CMSIS/Include/core_cmSimd.h \
+ libchip/include/samv71/system_samv71.h \
+ libchip/include/samv71/component/component_acc.h \
+ libchip/include/samv71/component/component_aes.h \
+ libchip/include/samv71/component/component_afec.h \
+ libchip/include/samv71/component/component_chipid.h \
+ libchip/include/samv71/component/component_dacc.h \
+ libchip/include/samv71/component/component_efc.h \
+ libchip/include/samv71/component/component_gmac.h \
+ libchip/include/samv71/component/component_gpbr.h \
+ libchip/include/samv71/component/component_hsmci.h \
+ libchip/include/samv71/component/component_icm.h \
+ libchip/include/samv71/component/component_isi.h \
+ libchip/include/samv71/component/component_matrix.h \
+ libchip/include/samv71/component/component_mcan.h \
+ libchip/include/samv71/component/component_mlb.h \
+ libchip/include/samv71/component/component_pio.h \
+ libchip/include/samv71/component/component_pmc.h \
+ libchip/include/samv71/component/component_pwm.h \
+ libchip/include/samv71/component/component_qspi.h \
+ libchip/include/samv71/component/component_rstc.h \
+ libchip/include/samv71/component/component_rswdt.h \
+ libchip/include/samv71/component/component_rtc.h \
+ libchip/include/samv71/component/component_rtt.h \
+ libchip/include/samv71/component/component_sdramc.h \
+ libchip/include/samv71/component/component_smc.h \
+ libchip/include/samv71/component/component_spi.h \
+ libchip/include/samv71/component/component_ssc.h \
+ libchip/include/samv71/component/component_supc.h \
+ libchip/include/samv71/component/component_tc.h \
+ libchip/include/samv71/component/component_trng.h \
+ libchip/include/samv71/component/component_twihs.h \
+ libchip/include/samv71/component/component_uart.h \
+ libchip/include/samv71/component/component_usart.h \
+ libchip/include/samv71/component/component_usbhs.h \
+ libchip/include/samv71/component/component_utmi.h \
+ libchip/include/samv71/component/component_wdt.h \
+ libchip/include/samv71/component/component_xdmac.h \
+ libchip/include/samv71/instance/instance_hsmci.h \
+ libchip/include/samv71/instance/instance_ssc.h \
+ libchip/include/samv71/instance/instance_spi0.h \
+ libchip/include/samv71/instance/instance_tc0.h \
+ libchip/include/samv71/instance/instance_tc1.h \
+ libchip/include/samv71/instance/instance_tc2.h \
+ libchip/include/samv71/instance/instance_twihs0.h \
+ libchip/include/samv71/instance/instance_twihs1.h \
+ libchip/include/samv71/instance/instance_pwm0.h \
+ libchip/include/samv71/instance/instance_usart0.h \
+ libchip/include/samv71/instance/instance_usart1.h \
+ libchip/include/samv71/instance/instance_usart2.h \
+ libchip/include/samv71/instance/instance_mcan0.h \
+ libchip/include/samv71/instance/instance_mcan1.h \
+ libchip/include/samv71/instance/instance_usbhs.h \
+ libchip/include/samv71/instance/instance_afec0.h \
+ libchip/include/samv71/instance/instance_dacc.h \
+ libchip/include/samv71/instance/instance_acc.h \
+ libchip/include/samv71/instance/instance_icm.h \
+ libchip/include/samv71/instance/instance_isi.h \
+ libchip/include/samv71/instance/instance_gmac.h \
+ libchip/include/samv71/instance/instance_tc3.h \
+ libchip/include/samv71/instance/instance_spi1.h \
+ libchip/include/samv71/instance/instance_pwm1.h \
+ libchip/include/samv71/instance/instance_twihs2.h \
+ libchip/include/samv71/instance/instance_afec1.h \
+ libchip/include/samv71/instance/instance_mlb.h \
+ libchip/include/samv71/instance/instance_aes.h \
+ libchip/include/samv71/instance/instance_trng.h \
+ libchip/include/samv71/instance/instance_xdmac.h \
+ libchip/include/samv71/instance/instance_qspi.h \
+ libchip/include/samv71/instance/instance_smc.h \
+ libchip/include/samv71/instance/instance_sdramc.h \
+ libchip/include/samv71/instance/instance_matrix.h \
+ libchip/include/samv71/instance/instance_utmi.h \
+ libchip/include/samv71/instance/instance_pmc.h \
+ libchip/include/samv71/instance/instance_uart0.h \
+ libchip/include/samv71/instance/instance_chipid.h \
+ libchip/include/samv71/instance/instance_uart1.h \
+ libchip/include/samv71/instance/instance_efc.h \
+ libchip/include/samv71/instance/instance_pioa.h \
+ libchip/include/samv71/instance/instance_piob.h \
+ libchip/include/samv71/instance/instance_pioc.h \
+ libchip/include/samv71/instance/instance_piod.h \
+ libchip/include/samv71/instance/instance_pioe.h \
+ libchip/include/samv71/instance/instance_rstc.h \
+ libchip/include/samv71/instance/instance_supc.h \
+ libchip/include/samv71/instance/instance_rtt.h \
+ libchip/include/samv71/instance/instance_wdt.h \
+ libchip/include/samv71/instance/instance_rtc.h \
+ libchip/include/samv71/instance/instance_gpbr.h \
+ libchip/include/samv71/instance/instance_rswdt.h \
+ libchip/include/samv71/instance/instance_uart2.h \
+ libchip/include/samv71/instance/instance_uart3.h \
+ libchip/include/samv71/instance/instance_uart4.h \
+ libchip/include/samv71/pio/pio_samv71q21.h
+
+libchip/include/samv71/samv71.h:
+
+libchip/include/samv71/samv71q21.h:
+
+/home/admin1/arm_toolchain/gcc-arm-none-eabi-6-2017-q2-update/lib/gcc/arm-none-eabi/6.3.1/include/stdint.h:
+
+/home/admin1/arm_toolchain/gcc-arm-none-eabi-6-2017-q2-update/arm-none-eabi/include/stdint.h:
+
+/home/admin1/arm_toolchain/gcc-arm-none-eabi-6-2017-q2-update/arm-none-eabi/include/machine/_default_types.h:
+
+/home/admin1/arm_toolchain/gcc-arm-none-eabi-6-2017-q2-update/arm-none-eabi/include/sys/features.h:
+
+/home/admin1/arm_toolchain/gcc-arm-none-eabi-6-2017-q2-update/arm-none-eabi/include/_newlib_version.h:
+
+/home/admin1/arm_toolchain/gcc-arm-none-eabi-6-2017-q2-update/arm-none-eabi/include/sys/_intsup.h:
+
+/home/admin1/arm_toolchain/gcc-arm-none-eabi-6-2017-q2-update/arm-none-eabi/include/sys/_stdint.h:
+
+libchip/include/cmsis/CMSIS/Include/core_cm7.h:
+
+libchip/include/cmsis/CMSIS/Include/core_cmInstr.h:
+
+libchip/include/cmsis/CMSIS/Include/cmsis_gcc.h:
+
+libchip/include/cmsis/CMSIS/Include/core_cmFunc.h:
+
+libchip/include/cmsis/CMSIS/Include/core_cmSimd.h:
+
+libchip/include/samv71/system_samv71.h:
+
+libchip/include/samv71/component/component_acc.h:
+
+libchip/include/samv71/component/component_aes.h:
+
+libchip/include/samv71/component/component_afec.h:
+
+libchip/include/samv71/component/component_chipid.h:
+
+libchip/include/samv71/component/component_dacc.h:
+
+libchip/include/samv71/component/component_efc.h:
+
+libchip/include/samv71/component/component_gmac.h:
+
+libchip/include/samv71/component/component_gpbr.h:
+
+libchip/include/samv71/component/component_hsmci.h:
+
+libchip/include/samv71/component/component_icm.h:
+
+libchip/include/samv71/component/component_isi.h:
+
+libchip/include/samv71/component/component_matrix.h:
+
+libchip/include/samv71/component/component_mcan.h:
+
+libchip/include/samv71/component/component_mlb.h:
+
+libchip/include/samv71/component/component_pio.h:
+
+libchip/include/samv71/component/component_pmc.h:
+
+libchip/include/samv71/component/component_pwm.h:
+
+libchip/include/samv71/component/component_qspi.h:
+
+libchip/include/samv71/component/component_rstc.h:
+
+libchip/include/samv71/component/component_rswdt.h:
+
+libchip/include/samv71/component/component_rtc.h:
+
+libchip/include/samv71/component/component_rtt.h:
+
+libchip/include/samv71/component/component_sdramc.h:
+
+libchip/include/samv71/component/component_smc.h:
+
+libchip/include/samv71/component/component_spi.h:
+
+libchip/include/samv71/component/component_ssc.h:
+
+libchip/include/samv71/component/component_supc.h:
+
+libchip/include/samv71/component/component_tc.h:
+
+libchip/include/samv71/component/component_trng.h:
+
+libchip/include/samv71/component/component_twihs.h:
+
+libchip/include/samv71/component/component_uart.h:
+
+libchip/include/samv71/component/component_usart.h:
+
+libchip/include/samv71/component/component_usbhs.h:
+
+libchip/include/samv71/component/component_utmi.h:
+
+libchip/include/samv71/component/component_wdt.h:
+
+libchip/include/samv71/component/component_xdmac.h:
+
+libchip/include/samv71/instance/instance_hsmci.h:
+
+libchip/include/samv71/instance/instance_ssc.h:
+
+libchip/include/samv71/instance/instance_spi0.h:
+
+libchip/include/samv71/instance/instance_tc0.h:
+
+libchip/include/samv71/instance/instance_tc1.h:
+
+libchip/include/samv71/instance/instance_tc2.h:
+
+libchip/include/samv71/instance/instance_twihs0.h:
+
+libchip/include/samv71/instance/instance_twihs1.h:
+
+libchip/include/samv71/instance/instance_pwm0.h:
+
+libchip/include/samv71/instance/instance_usart0.h:
+
+libchip/include/samv71/instance/instance_usart1.h:
+
+libchip/include/samv71/instance/instance_usart2.h:
+
+libchip/include/samv71/instance/instance_mcan0.h:
+
+libchip/include/samv71/instance/instance_mcan1.h:
+
+libchip/include/samv71/instance/instance_usbhs.h:
+
+libchip/include/samv71/instance/instance_afec0.h:
+
+libchip/include/samv71/instance/instance_dacc.h:
+
+libchip/include/samv71/instance/instance_acc.h:
+
+libchip/include/samv71/instance/instance_icm.h:
+
+libchip/include/samv71/instance/instance_isi.h:
+
+libchip/include/samv71/instance/instance_gmac.h:
+
+libchip/include/samv71/instance/instance_tc3.h:
+
+libchip/include/samv71/instance/instance_spi1.h:
+
+libchip/include/samv71/instance/instance_pwm1.h:
+
+libchip/include/samv71/instance/instance_twihs2.h:
+
+libchip/include/samv71/instance/instance_afec1.h:
+
+libchip/include/samv71/instance/instance_mlb.h:
+
+libchip/include/samv71/instance/instance_aes.h:
+
+libchip/include/samv71/instance/instance_trng.h:
+
+libchip/include/samv71/instance/instance_xdmac.h:
+
+libchip/include/samv71/instance/instance_qspi.h:
+
+libchip/include/samv71/instance/instance_smc.h:
+
+libchip/include/samv71/instance/instance_sdramc.h:
+
+libchip/include/samv71/instance/instance_matrix.h:
+
+libchip/include/samv71/instance/instance_utmi.h:
+
+libchip/include/samv71/instance/instance_pmc.h:
+
+libchip/include/samv71/instance/instance_uart0.h:
+
+libchip/include/samv71/instance/instance_chipid.h:
+
+libchip/include/samv71/instance/instance_uart1.h:
+
+libchip/include/samv71/instance/instance_efc.h:
+
+libchip/include/samv71/instance/instance_pioa.h:
+
+libchip/include/samv71/instance/instance_piob.h:
+
+libchip/include/samv71/instance/instance_pioc.h:
+
+libchip/include/samv71/instance/instance_piod.h:
+
+libchip/include/samv71/instance/instance_pioe.h:
+
+libchip/include/samv71/instance/instance_rstc.h:
+
+libchip/include/samv71/instance/instance_supc.h:
+
+libchip/include/samv71/instance/instance_rtt.h:
+
+libchip/include/samv71/instance/instance_wdt.h:
+
+libchip/include/samv71/instance/instance_rtc.h:
+
+libchip/include/samv71/instance/instance_gpbr.h:
+
+libchip/include/samv71/instance/instance_rswdt.h:
+
+libchip/include/samv71/instance/instance_uart2.h:
+
+libchip/include/samv71/instance/instance_uart3.h:
+
+libchip/include/samv71/instance/instance_uart4.h:
+
+libchip/include/samv71/pio/pio_samv71q21.h:
--- dir/src/main.cpp
+++ dir/src/main.cpp
@@ -1,25 +1,33 @@
-#include <stdio.h>
+#include <samv71/samv71.h>
+
 extern "C" void SystemInit()
 {
 }
 
+void dalay1();
+int main ()
+{  
+        dalay1();
+     
+    *((unsigned long int*)0x400E0610) = 0x400;//включает блок PIOA
+    *((unsigned long int*)0x400E0E00) = 0x800000;//активровали пин
+    *((unsigned long int*)0x400E0EA4) = 0x800000;//разрешает изменение
+    *((unsigned long int*)0x400E0E10) = 0x800000;//активирует линию входа/выхода на пине
 
-int main () {
-                
-                            *((unsigned long int*)0x400E0610) = 0x400;//включает блок PIOA
-                            *((unsigned long int*)0x400E0E00) = 0x800000;//активровали пин
-                            *((unsigned long int*)0x400E0E10) = 0x800000;//активирует линию входа/выхода на пине
-
-                            delay(1000);
-
-
-
-                while(1){
-                            *((unsigned long int*)0x400E0EA4) = 0x800000;//деактивирует линию на пине 23
-                            delay(1000);
-                            *((unsigned long int*)0x400E0E10) = 0x800000;//активирует линию входа/выхода на пине 23
-                            delay(1000);
-                        }
+    while(1)
+    {
+    *((unsigned long int*)0x400E0E34) = 0x800000;//clear
+     dalay1();   
+    *((unsigned long int*)0x400E0E30) = 0x800000;//set
+     dalay1();
+    }
+     
+}
 
 
-            }
+void dalay1(){
+    for(volatile long long int i = 0; i<100000;++i){
+        asm volatile ("nop");
+    }
+}
+
