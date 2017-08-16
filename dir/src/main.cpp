737aa3d2dc15e10f5fd0b1414d9ed922738c2c3b
Michael <go99@bk.ru>
2017-08-16 18:30:17 +0300

    код отформатирован, адреса заменены

--- dir/src/main.cpp
+++ dir/src/main.cpp
@@ -11,26 +11,28 @@ int main ()
 {  
 
     SCB_EnableICache(); // Instruction cache Enable
+    
     SCB_EnableDCache(); // Data cache Enable
 
-  
-        delay1();
-        
-    * ( ( unsigned long int * ) 0x400E0630 ) = 0x2;  //включает тактирование через PLLACK
-    * ( ( unsigned long int * ) 0x400E0628 ) = 0x20180001;  //ставит умножитель PLLACK на 25 и делитель на 1 + бит one + 0 циклов медленнх часов
+    REG_PMC_MCKR  = 0x2;  //включает тактирование через PLLACK
+     
+    REG_CKGR_PLLAR  = 0x20180001;  //ставит умножитель PLLACK на 25 и делитель на 1 + бит one + 0 циклов медленнх часов
     
+    REG_PMC_PCER0 = 0x400; //включает блок PIOA
+    
+    // PIOA = 0x800000; //активровали пин в блоке PIOA   ----------не работает!
+    * ( ( unsigned long int * ) 0x400E0E00 ) = 0x800000; //активровали пин в блоке PIOA
 
-    * ( ( unsigned long int * ) 0x400E0610 ) = 0x400;//включает блок PIOA
-    * ( ( unsigned long int * ) 0x400E0E00 ) = 0x800000;//активровали пин
-    * ( ( unsigned long int * ) 0x400E0EA4 ) = 0x800000;//разрешает изменение
-    * ( ( unsigned long int * ) 0x400E0E10 ) = 0x800000;//активирует линию входа/выхода на пине
+    REG_PIOA_OWDR = 0x800000; //разрешает изменение
+    
+    REG_PIOA_OER = 0x800000; //активирует линию входа/выхода на пине
 
     while(1)
     {
-    * ( ( unsigned long int * ) 0x400E0E34 ) = 0x800000;//clear
-     delay1();   
-    * ( ( unsigned long int * ) 0x400E0E30 ) = 0x800000;//set
-     delay1();
+        REG_PIOA_CODR = 0x800000; //clear
+        delay1();  
+        REG_PIOA_SODR = 0x800000; //set
+        delay1();
     }
      
 }
@@ -47,4 +49,16 @@ void delay1(){
 
 
 }
+/*
+ *  PIOA = 0x400E0E00             файл  samv71q21.h
+ *  REG_PMC_MCKR = 0x400E0630     файл  instance_pmc.h
+ *  REG_CKGR_PLLAR = 0x400E0628   файл  instance_pmc.h
+ *  REG_PMC_PCER0  = 0x400E0610   файл  instance_pmc.h
+ *  REG_PIOA_OWDR = 0x400E0EA4    файл  instance_pioa.h
+ *  REG_PIOA_OER = 0x400E0E10     файл  instance_pioa.h
+ *  REG_PIOA_CODR = 0x400E0E34    файл  instance_pioa.h
+ *  REG_PIOA_SODR = 0x400E0E30    файл  instance_pioa.h
+ * 
+ * 
+ * instance_pioa.h  свойства регистра блока выходов a*/
 
