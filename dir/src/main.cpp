86d20e9856f0cea86465c4e29e21c991320e122c
Michael <go99@bk.ru>
2017-08-19 17:48:54 +0300

    заменены захардкорженные адреса, кроме PLL

--- dir/src/main.cpp
+++ dir/src/main.cpp
@@ -14,24 +14,24 @@ int main ()
     
     SCB_EnableDCache(); // Data cache Enable
 
-    REG_PMC_MCKR  = 0x2;  //включает тактирование через PLLACK
+    PMC->PMC_MCKR = PMC_MCKR_CSS_PLLA_CLK;  //включает тактирование через PLLACK
      
     REG_CKGR_PLLAR  = 0x20180001;  //ставит умножитель PLLACK на 25 и делитель на 1 + бит one + 0 циклов медленнх часов
     
-    REG_PMC_PCER0 = 0x400; //включает блок PIOA
+    PMC->PMC_PCER0 = PMC_PCER0_PID10; //включает тактирование периферии включая PIOA
     
-    // PIOA = 0x800000; //активровали пин в блоке PIOA   ----------не работает!
-    * ( ( unsigned long int * ) 0x400E0E00 ) = 0x800000; //активровали пин в блоке PIOA
+    PIOA->PIO_PER=PIO_PA23; //активровали пин в блоке PIOA  (через смещение в файле)        * ( ( unsigned long int * ) 0x400E0E00 ) = 0x800000;
 
-    REG_PIOA_OWDR = 0x800000; //разрешает изменение
+    PIOA->PIO_OWDR=PIO_PA23; //REG_PIOA_OWDR = PIO_PA23; //разрешает изменение PIO_OWDR
     
-    REG_PIOA_OER = 0x800000; //активирует линию входа/выхода на пине
+    PIOA->PIO_OER=PIO_PA23;  //REG_PIOA_OER = PIO_PA23; //активирует линию входа/выхода на пине
+
 
     while(1)
     {
-        REG_PIOA_CODR = 0x800000; //clear
+        PIOA->PIO_CODR = PIO_PA23; //clear
         delay1();  
-        REG_PIOA_SODR = 0x800000; //set
+        PIOA->PIO_SODR = PIO_PA23; //set
         delay1();
     }
      
@@ -39,7 +39,7 @@ int main ()
 
 
 void delay1(){
-    for ( volatile long long int i = 0; i < 1000000; ++ i )
+    for ( volatile long long int i = 0; i < 10000000; ++ i )
     {
         asm volatile ( "nop" );
     }
@@ -54,6 +54,7 @@ void delay1(){
  *  REG_PMC_MCKR = 0x400E0630     файл  instance_pmc.h
  *  REG_CKGR_PLLAR = 0x400E0628   файл  instance_pmc.h
  *  REG_PMC_PCER0  = 0x400E0610   файл  instance_pmc.h
+ * 
  *  REG_PIOA_OWDR = 0x400E0EA4    файл  instance_pioa.h
  *  REG_PIOA_OER = 0x400E0E10     файл  instance_pioa.h
  *  REG_PIOA_CODR = 0x400E0E34    файл  instance_pioa.h
